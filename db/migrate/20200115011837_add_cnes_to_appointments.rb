500c35a4b0d7a337a7bfdec50ae7f0160ed95c92
Yuri Coelho <ycr.data@gmail.com>
2020-01-14 22:47:40 -0300

    Corrigindo associações entre as tabelas

---
+++ db/migrate/20200115011837_add_cnes_to_appointments.rb
@@ -0,0 +1,4 @@
+class AddCnesToAppointments < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
---
+++ db/migrate/20200115012206_remove_patient_id_from_cnes.rb
@@ -0,0 +1,5 @@
+class RemovePatientIdFromCnes < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :cnes, :patient_id
+  end
+end
