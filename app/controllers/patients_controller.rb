67093ab5888eb5f96282dbed447d4ca8cf03f9f9
Yuri Coelho <ycr.data@gmail.com>
2020-03-26 00:02:49 -0300

    Controllers de pacientes e profissionais implementados

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,24 +3,50 @@ class PatientsController < ApplicationController
   end
 
   def create
+    patient = Patient.new(patient_params)
+      
+    if patient.save
+      render json: { status: "SUCCESS", message: "You created a new patient in the database. See below", data: patient }, status: :ok
+    else
+      render json: { status: "ERROR", message: "You were unable to save a new patient. Please try again", data: patient.errors }, status: :unprocessable_entity
+    end
   end
 
   def update
+    patient = Patient.find(params[:id])
+
+    if patient.update_attributes(patient_params)
+      render json: { status: "SUCCESS", message: "You updated the patient record in the database", data: patient }, status: :ok
+    else
+      render json: { status: "ERROR", message: "You were unable to update the patient record. Please try again", data: patien.errors }, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def destroy
+    patient = Patient.find(params[:id])
+
+    patient.destroy
+    render json: { status: "SUCCESS", message: "You deleted a patient record", data: patient }, status: :ok
   end
 
   def show
     patient = Patient.find(params[:id])
-    render json: {data: patient}, status: :ok
+
+    render json: { status: "SUCCESS", message: "See the information of your loaded patient below", data: patient }, status: :ok
   end
 
   def index
     patients = Patient.order(:id)
-    render json: {data: patients}, status: :ok
+
+    render json: { status: "SUCCESS", message: "See all patients below", data: patients }, status: :ok
   end
+end
+
+private
+def patient_params
+  params.require(:patient).permit(:medical_record_number, :name, :gender, :birthdate, :admission_date, :age, :marital_status, :mother_name, :father_name, 
+  :sus_card, :cpf, :rg, :county, :adress, :phone, :cnes_id, :status, :agent_id)
 end
---
+++ app/controllers/professionals_controller.rb
@@ -0,0 +1,53 @@
+class ProfessionalsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def new
+    end
+  
+    def create
+        professional = Professional.new(professional_params)
+
+        if professional.save
+            render json: { status: "SUCCESS", message: "You created a new professional in the database. See below", data: professional }, status: :ok
+        else 
+            render json: { status: "ERROR", message: "You were unable to save a new professional. Please try again", data: professional.errors }, status: :unprocessable_entity
+        end
+    end
+  
+    def update
+        professional = Professional.find(params[:id])
+
+        if professional.update_attributes
+            render json: { status: "SUCCESS", message: "You updated the professional record in the database", data: professional }, status: :ok
+        else
+            render json: { status: "ERROR", message: "You were unable to update the professional record. Please try again", data: professional.errors }, status: :unprocessable_entity
+        end
+    end
+  
+    def edit
+    end
+  
+    def destroy
+        professional = Professional.find(params[:id])
+
+        professional.destroy
+        render json: { status: "SUCCESS", message: "You deleted a professional record", data: professional }, status: :ok
+    end
+  
+    def show
+        professional = Professional.find(params[:id])
+
+        render json: { status: "SUCCESS", message: "See the information of your loaded professional below", data: professional }, status: :ok
+    end
+  
+    def index
+        professionals = Professional.order(:id)
+
+        render json: { status: "SUCESS", message: "See all professionals below", data: professionals }, status: :ok
+    end
+
+    private
+    def professional_params
+        params.require(:professional).permit(:name, :function)
+    end
+end
